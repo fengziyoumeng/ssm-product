52576766b904d71f764beb51d442a536deec30fb
fengziyoumeng <995529324@qq.com>
2018-01-22 23:06:05 +0800

    当当+aop切换数据源

---
+++ src/main/java/com/cn/wubin/sharding/DataSourceInterceptor.java
@@ -0,0 +1,34 @@
+package com.cn.wubin.sharding;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.aspectj.lang.JoinPoint;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataSourceInterceptor {
+
+    private static final Logger logger = LogManager.getLogger(DataSourceInterceptor.class);
+
+    public void setdataSourceOne(JoinPoint jp) {
+        if( !"dataSourceRDB".equals(  DatabaseContextHolder.getCustomerType() ))
+        {
+            //if(logger.isDebugEnabled()){
+                logger.info("==========>设置为dataSourceRDB数据源,"+jp.getSignature().toString());
+                logger.info("========>设置为dataSourceRDB数据源,原先数据源名称为："+ DatabaseContextHolder.getCustomerType());
+           // }
+            DatabaseContextHolder.setCustomerType("dataSourceRDB");
+        }
+
+    }
+
+    public void setdataSourceTwo(JoinPoint jp) {
+        if( !"dataSourceNoSharding".equals(  DatabaseContextHolder.getCustomerType() )){
+           // if(logger.isDebugEnabled()){
+            logger.info("==========>设置为dataSourceNoSharding数据源,"+jp.getSignature().toString());
+                logger.info("========>设置为dataSourceNoSharding数据源,原先数据源名称为："+ DatabaseContextHolder.getCustomerType());
+            //}
+            DatabaseContextHolder.setCustomerType("dataSourceNoSharding");
+        }
+    }
+}

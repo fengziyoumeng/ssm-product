457fabe6c31d2bd97e1206a08f17b679f6471aab
fengziyoumeng <995529324@qq.com>
2018-01-26 15:23:41 +0800

    实现一个dubbo-1.0简易版： 利用socket流和jdk动态代理来通信拿到接口。
    出现JSON转换错误时只需要升级到最新版本就行：
    com.alibaba.fastjson.JSONException: not close json text, token : error

---
+++ src/main/java/com/cn/wubin/StudyConsumer.java
@@ -0,0 +1,48 @@
+package com.cn.wubin;
+
+import com.alibaba.fastjson.JSON;
+import com.cn.wubin.model.pojo.BannerInfo;
+import com.cn.wubin.service.IBannerInfoService;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.net.Socket;
+
+public class StudyConsumer {
+
+    public static void main(String[] args) {
+        IBannerInfoService bannerInfoService = (IBannerInfoService) remote(IBannerInfoService.class);
+        BannerInfo bannerInfo = bannerInfoService.queryBannerInfo(1l);
+        System.out.println("客户端收到结果："+bannerInfo);
+    }
+
+    private static Object remote(final Class clazz) {
+
+        return Proxy.newProxyInstance(clazz.getClassLoader(), new Class[]{clazz}, new InvocationHandler() {
+            @Override
+            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+
+                Socket socket = new Socket("127.0.0.1",6666);
+                OutputStream os = socket.getOutputStream();
+                os.write(String.valueOf(args[0]).getBytes());
+                os.flush();
+
+                InputStream in = socket.getInputStream();
+                byte[] b = new byte[1024];
+                in.read(b);
+
+                String s = new String(b).trim();
+                System.out.println("接口返回值："+s);
+                BannerInfo bannerInfo = JSON.parseObject(new String(b),BannerInfo.class);
+
+                in.close();
+                os.close();
+                socket.close();
+                return bannerInfo;
+            }
+        });
+    }
+}
---
+++ src/main/java/com/cn/wubin/StudyProvider.java
@@ -0,0 +1,60 @@
+package com.cn.wubin;
+
+import com.alibaba.fastjson.JSONObject;
+import com.cn.wubin.model.pojo.BannerInfo;
+import com.cn.wubin.service.IBannerInfoService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StudyProvider {
+
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:*-beans.xml");
+        context.start();
+
+        //解决网络通讯（ServerSocket通信效率不高，之后可以替换）
+        try {
+            ServerSocket serverSocket = new ServerSocket(6666);
+            while (true){
+                Socket socket = serverSocket.accept();
+                InputStream is = socket.getInputStream();
+                byte[] b = new byte[16];
+                is.read(b);
+
+                System.out.println("服务器收到请求："+new String(b));
+                IBannerInfoService bannerInfoService = (IBannerInfoService) context.getBean("bannerInfoService");
+                Long l = Long.parseLong(getNumbers(new String(b)));
+                System.out.println("请求值l："+new String(b));
+                BannerInfo bannerInfo = bannerInfoService.queryBannerInfo(l);
+                System.out.println("接口实现类返回值："+JSONObject.toJSONString(bannerInfo));
+
+                OutputStream os = socket.getOutputStream();
+                os.write(JSONObject.toJSONString(bannerInfo).getBytes());
+                os.flush();
+
+                os.close();
+                is.close();
+                socket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //截取数字  【读取字符串中第一个连续的字符串，不包含后面不连续的数字】
+    public static String getNumbers(String content) {
+        Pattern pattern = Pattern.compile("\\d+");
+        Matcher matcher = pattern.matcher(content);
+        while (matcher.find()) {
+            return matcher.group(0);
+        }
+        return "";
+    }
+}
